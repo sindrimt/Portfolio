70ed0accca6dcc12263446d495e59ef9b8f1d665
sindrimt <sindrimt@gmail.com>
2022-04-09 13:07:09 +0200

    Removed ts from TimeLine

--- src/components/TimeLine/TimeLine.jsx
+++ src/components/TimeLine/TimeLine.jsx
@@ -26,13 +26,9 @@ import { norwegian, english } from "../../constants/language";
 
 const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
 
-interface Props {
-  scrollWidth?: number;
-}
-
 const Timeline = () => {
   const [activeItem, setActiveItem] = useState(0);
-  const carouselRef = useRef<HTMLDivElement>(null);
+  const carouselRef = useRef();
 
   const scroll = (node, left) => {
     return node.scrollTo({ left, behavior: "smooth" });
