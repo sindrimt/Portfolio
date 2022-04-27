02e162589a21a2f74686806c864306220066d7ef
sindrimt <sindrimt@gmail.com>
2022-04-27 19:45:05 +0200

    darkmode default

--- context/ContextState.js
+++ context/ContextState.js
@@ -3,12 +3,10 @@ import React, { useState, createContext } from "react";
 export const ContextState = createContext();
 
 export const StateProvider = (props) => {
-  const [darkMode, setDarkMode] = useState(false); // Default som light mode
+  const [darkMode, setDarkMode] = useState(true); // Default som light mode
   const [language, setLanguage] = useState(true); // false standard for norsk
   return (
-    <ContextState.Provider
-      value={[darkMode, setDarkMode, language, setLanguage]}
-    >
+    <ContextState.Provider value={[darkMode, setDarkMode, language, setLanguage]}>
       {props.children} {/* Rendrer alle components inni ContextState */}
     </ContextState.Provider>
   );
