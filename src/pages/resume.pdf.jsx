22b67f6470f44a038fec836c1477c4d369ca180d
sindrimt <sindrimt@gmail.com>
2022-04-29 16:48:33 +0200

    resume fil som NextJs page

---
+++ src/pages/resume.pdf.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function FirstPost() {
+  return (
+    <Wrapper>
+      <embed src="/images/oving.pdf" type="application/pdf" width="100%" height="99.8%" />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+`;
